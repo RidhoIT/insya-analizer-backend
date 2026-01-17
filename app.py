57b12627b9fe9b7aa539b7638d16c8ca733281d0
Muhammad Ridho <ridho22ti@mahasiswa.pcr.ac.id>
2026-01-17 23:53:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS
 
 # 🔑 Multiple API Keys for fallback - ganti dengan API keys milikmu
 GEMINI_API_KEYS = [
-         "AIzaSyB4Y1t9GBsnueguZA7mGT0uvhgKfvNPMvw",
+    
 
-     # "AIzaSyDvo1FDQbtVtLxpGk1E40_xE0wv3xtpuys",
+     "AIzaSyDvo1FDQbtVtLxpGk1E40_xE0wv3xtpuys",
      "AIzaSyAJl7pwh_Hj5fmRFtQl6T14ZkiTzdrautQ", 
     "AIzaSyDvo1FDQbtVtLxpGk1E40_xE0wv3xtpuys"
    
